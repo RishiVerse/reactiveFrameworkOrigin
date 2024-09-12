a62ef28ff542b11a7a9d8a967268d06a3bb4cdb5
Rishabh <121147514+RishiVerse@users.noreply.github.com>
2024-09-12 21:54:37 +0530

    Initial commit

---
+++ src/main/java/com/example/reactive/reactiveFramework/ReactiveFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactive.reactiveFramework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reactive/reactiveFramework/ReactiveFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactive.reactiveFramework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
